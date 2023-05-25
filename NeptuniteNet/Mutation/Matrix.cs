36273f5d3bda78c6064bbcf5c9f59a3176a9412b
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-25 18:08:19 +0530

    Add function to mutate mask matrix

---
+++ NeptuniteNet/Mutation/Matrix.cs
@@ -0,0 +1,26 @@
+﻿using Neptunite.Configuration;
+
+namespace Neptunite.Mutation;
+internal static class Matrix
+{
+  // TODO: Make multi-threading safe
+  static readonly Random random = new();
+
+  public static void MutateInplace(ref sbyte[][] matrix, in ParameterSchema parameter)
+  {
+    int height = matrix.Length;
+    int width = matrix[0].Length;
+
+    for (int i = 0; i < height; i++)
+    {
+      for (int j = 0; j < width; j++)
+      {
+        if (parameter.MutationThreshold > random.NextDouble()) { matrix[i][j] = (sbyte)GenerateRandomMaskElement(); }
+      }
+    }
+  }
+
+  const int MinMaskValue = -5;
+  const int MaxMaskValue = 5;
+  private static int GenerateRandomMaskElement() => random.Next(MinMaskValue, MaxMaskValue);
+}

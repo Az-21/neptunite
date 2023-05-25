2fb1d7c03f0166feb6f8990908e29a31f831a64b
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-25 20:36:47 +0530

    Add Sobel edge detection matrices

---
+++ NeptuniteNet/Static/EdgeDetection.cs
@@ -0,0 +1,16 @@
+﻿namespace Neptunite.Static;
+internal static class EdgeDetection
+{
+  static readonly sbyte[][] SobelMatrixVertical = new sbyte[][] {
+  new sbyte[] { -1, -2, -1 },
+  new sbyte[] { 0, 0, 0 },
+  new sbyte[] { 1, 2, 1 },
+  };
+
+  static readonly sbyte[][] SobelMatrixHorizontal = new sbyte[][] {
+  new sbyte[] { -1, 0, 1 },
+  new sbyte[] { -2, 0, 2 },
+  new sbyte[] { -1, 0, 1 },
+  };
+
+}

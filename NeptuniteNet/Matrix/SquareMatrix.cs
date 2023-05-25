d58bc9dd346bfb4df37a59c7698d0e70dda70a43
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-25 12:46:24 +0530

    Add square matrix generator

---
+++ NeptuniteNet/Matrix/SquareMatrix.cs
@@ -0,0 +1,15 @@
+﻿namespace Neptunite.Matrix;
+internal static class SquareMatrix
+{
+  public static byte[][] Create(byte dimension, byte fillWith = 0)
+  {
+    byte[][] matrix = new byte[dimension][];
+    for (int i = 0; i < dimension; i++)
+    {
+      byte[] row = new byte[dimension];
+      for (int j = 0; j < dimension; j++) { row[j] = fillWith; }
+    }
+
+    return matrix;
+  }
+}

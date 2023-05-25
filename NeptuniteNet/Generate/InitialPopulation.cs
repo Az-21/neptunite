91fcbe34e9ac03ce3ace7cead08de07ee52bae77
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-25 20:17:35 +0530

    Add initial population convolution matrix

---
+++ NeptuniteNet/Generate/InitialPopulation.cs
@@ -0,0 +1,20 @@
+﻿using Neptunite.Configuration;
+
+namespace Neptunite.Generate;
+internal static class InitialPopulation
+{
+  public static sbyte[][] GenerateRandomConvolutionMatrix(in ParameterSchema parameter)
+  {
+    byte dimension = parameter.ConvolutionMatrixDimension;
+    sbyte[][] matrix = Matrix.SquareMatrix.Create(dimension);
+    for (int i = 0; i < dimension; i++)
+    {
+      for (int j = 0; j < dimension; j++)
+      {
+        matrix[i][j] = Mutation.Matrix.GenerateRandomMaskElement();
+      }
+    }
+
+    return matrix;
+  }
+}

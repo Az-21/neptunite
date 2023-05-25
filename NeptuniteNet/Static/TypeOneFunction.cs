104748062de180ed3a7b493b75b23f824d1f6216
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-25 20:51:27 +0530

    Add framework for Type-I functions

---
+++ NeptuniteNet/Static/TypeOneFunction.cs
@@ -0,0 +1,17 @@
+﻿namespace Neptunite.Static;
+internal static class TypeOneFunction
+{
+  private static void Apply(ref int[][] matrix, Func<int, int> function)
+  {
+    for (int i = 0; i < matrix.Length; i++)
+    {
+      for (int j = 0; j < matrix[0].Length; j++)
+      {
+        matrix[i][j] = function(matrix[i][j]);
+      }
+    }
+  }
+
+  static readonly Func<int, int> LogBase10 = (x) => (int)Math.Log10(x);
+  public static void LogBase10Inplace(ref int[][] matrix) => Apply(ref matrix, LogBase10);
+}

2cf1868f3f76cc914a95aea3d2189897e21b3923
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-25 16:44:00 +0530

    Add convolution matrix dimension parameter

--- NeptuniteNet/Configuration/Parameters.cs
+++ NeptuniteNet/Configuration/Parameters.cs
@@ -7,7 +7,8 @@ public readonly record struct ParameterSchema(
   ushort IterationLimit,
   byte PopulationSize,
   string DatasetWithFeature,
-  string DatasetWithoutFeature
+  string DatasetWithoutFeature,
+  byte ConvolutionMatrixDimension
 );
 
 internal static class Parameters
@@ -29,14 +30,32 @@ internal static class Parameters
 
   public static void PrintParameters(in ParameterSchema parameter)
   {
+    VerifyParameters(in parameter);
+
+    string dim = parameter.ConvolutionMatrixDimension.ToString();
+    string convolutionMatrixDimension = $"{dim}x{dim}";
+
     AnsiConsole.Write(new Table()
-      .Border(TableBorder.Rounded)
-      .AddColumn("[green]Parameter[/]")
-      .AddColumn("[green]Value[/]")
-      .AddRow("Iteration limit", parameter.IterationLimit.ToString())
-      .AddRow("Population size", parameter.PopulationSize.ToString())
-      .AddRow("Path of training dataset with feature", parameter.DatasetWithFeature)
-      .AddRow("Path of training dataset without feature", parameter.DatasetWithoutFeature)
-    );
+        .Border(TableBorder.Rounded)
+        .AddColumn("[green]Parameter[/]")
+        .AddColumn("[green]Value[/]")
+        .AddRow("Iteration limit", parameter.IterationLimit.ToString())
+        .AddRow("Population size", parameter.PopulationSize.ToString())
+        .AddRow("Convolution matrix dimension", convolutionMatrixDimension)
+        .AddRow("Path of training dataset with feature", parameter.DatasetWithFeature)
+        .AddRow("Path of training dataset without feature", parameter.DatasetWithoutFeature)
+      );
   }
+
+  private static void VerifyParameters(in ParameterSchema parameter)
+  {
+    if (!VerifyConvolutionMatrixDimension(parameter.ConvolutionMatrixDimension))
+    {
+      AnsiConsole.MarkupLine("[red]FATAL[/]");
+      AnsiConsole.MarkupLine("Convolution matrix dimension must be odd");
+      Environment.Exit(0);
+    }
+  }
+
+  private static bool VerifyConvolutionMatrixDimension(in byte dimension) => dimension % 2 != 0;
 }

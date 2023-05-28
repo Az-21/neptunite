7b1542d6b64dfbb7d7a8b34ec1d3f07fbd1c6e89
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-28 15:30:17 +0530

    Add RNG based chromosome mutators for T1 and T2 chromosomes

---
+++ NeptuniteNet/Mutation/Chromosome.cs
@@ -0,0 +1,23 @@
+﻿using Neptunite.Configuration;
+
+namespace Neptunite.Mutation;
+internal static partial class Chromosome
+{
+  public static void MutateTypeOneChromosomeInplace(ref int[] t1Chromosome, in ParameterSchema parameter)
+  {
+    for (int i = 0; i < Generate.Chromosome.TypeOneChromosomeLength; i++)
+    {
+      if (Random.Shared.NextDouble() < parameter.MutationThreshold) { continue; }
+      t1Chromosome[i] = Random.Shared.Next(0, Static.TypeOneFunction.t1Count);
+    }
+  }
+
+  public static void MutateTypeTwoChromosomeInplace(ref int[] t2Chromosome, in ParameterSchema parameter)
+  {
+    for (int i = 0; i < Generate.Chromosome.TypeTwoChromosomeLength; i++)
+    {
+      if (Random.Shared.NextDouble() < parameter.MutationThreshold) { continue; }
+      t2Chromosome[i] = Random.Shared.Next(0, Static.TypeTwoFunction.t2Count);
+    }
+  }
+}

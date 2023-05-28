c3cf8bb81bcf9bb9b4cf939912dfcfeb38ae0d20
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-28 13:52:01 +0530

    Add fixed tree structure
    Dynamic tree can be implemented in v2

---
+++ NeptuniteNet/Tree/Structure.cs
@@ -0,0 +1,22 @@
+﻿namespace Neptunite.Tree;
+/*
+                     Dot
+                   /     \
+                T2F       T2F
+               /   \     /   \
+           T2F       T2F       T2F
+          /   \     /   \     /   \
+      T2F       T2F      T2F        T2F
+     /   \    /    \    /    \    /    \
+ T1F       T1F       T1F       T1F       T1F
+  |         |         |         |         |
+Convo     Convo     Convo     Convo     Convo
+  |         |         |         |         |
+Image     Image     Image     Image     Image
+*/
+
+internal static class Structure
+
+{
+
+}

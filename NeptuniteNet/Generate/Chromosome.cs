4e7581b35ec20bdfe80f978f70f3819d77b69ad9
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-28 14:28:07 +0530

    Add random chromosome generator for T1 and T2 functions

---
+++ NeptuniteNet/Generate/Chromosome.cs
@@ -0,0 +1,30 @@
+﻿using static Neptunite.Static.TypeOneFunction;
+using static Neptunite.Static.TypeTwoFunction;
+
+namespace Neptunite.Generate;
+internal static class Chromosome
+{
+  // NOTE: See Tree/Structure.cs for placeholders. Items are arranged in BFS manner.
+  const int TypeOneFunctionChromosomeLength = 5;
+  const int TypeTwoFunctionChromosomeLength = 9;
+
+  // Generate chromosome of fixed length. Fill values with T1F and T2F enums.
+  private static int[] FillChromosomeWithRandomIntegers(in int length, in int enumCount)
+  {
+    int[] chromosome = new int[length];
+    for (int i = 0; i < length; i++) { chromosome[i] = Random.Shared.Next(0, enumCount); }
+    return chromosome;
+  }
+
+  public static int[] RandomlyGenerateTypeOneChromosome()
+  {
+    int enumCount = Enum.GetNames(typeof(TypeOneOperation)).Length;
+    return FillChromosomeWithRandomIntegers(0, enumCount);
+  }
+
+  public static int[] RandomlyGenerateTypeTwoChromosome()
+  {
+    int enumCount = Enum.GetNames(typeof(TypeTwoOperation)).Length;
+    return FillChromosomeWithRandomIntegers(0, enumCount);
+  }
+}

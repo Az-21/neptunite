47fe40a6ac76be1e7f33d1da7d8b093c9464879b
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-28 23:59:30 +0530

    Create matrix image class

---
+++ NeptuniteNet/Image/Matrix.cs
@@ -0,0 +1,18 @@
+﻿namespace Neptunite.Image;
+internal class Matrix
+{
+  public int Height { get; }
+  public int Width { get; }
+  public int[][] Data { get; set; }
+
+  public Matrix(int[][] data)
+  {
+    Height = data.Length;
+    Width = data[0].Length;
+    Data = data;
+  }
+
+  public void Modify(int value, int i, int j) => Data[i][j] = value;
+  public int ReadValue(int i, int j) => Data[i][j];
+  public int[] ReadRow(int i) => Data[i];
+}

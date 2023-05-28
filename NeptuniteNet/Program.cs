ebd198c69713a8962722a82fc565bf53c0e9ee4a
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-28 20:53:14 +0530

    Update Program.cs

--- NeptuniteNet/Program.cs
+++ NeptuniteNet/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Neptunite.Configuration;
+using Neptunite.Generate;
 using Neptunite.Image;
+using Neptunite.Tree;
 
 namespace Neptunite;
 internal static class Program
@@ -13,5 +15,21 @@ internal static class Program
     // 2. Load path of all `.jpg` and `.jpeg` files into memory
     Images images = Load.PathOfImages(in parameter);
     Load.PrintImagesCount(images);
+
+    // 3. Generate initial population of chromosomes (T1F, T2F, and convolution)
+    Pop[] population = Population.GenerateInitialPopulation(in parameter);
+
+    // 4. Evaluate every chromosome against every image -> Select top 50% -> Mutate -> Repeat
+    for (int i = 0; i < parameter.IterationLimit; i++)
+    {
+      int[] fitness = Structure.EvaluatePopulation(in population, in images);
+
+      // TODO
+      // - Linked sort
+      // - Eliminate bottom half
+      // - Mutate and append top half
+    }
+
+    // TODO: Output final interation chromosome
   }
 }

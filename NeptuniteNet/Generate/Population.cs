87722d248577c20a7bfcab425497ece5d12b2a54
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-28 15:00:29 +0530

    Add initial population constructor

---
+++ NeptuniteNet/Generate/Population.cs
@@ -0,0 +1,21 @@
+﻿using Neptunite.Configuration;
+
+namespace Neptunite.Generate;
+internal record class Pop(int[] T1Chromosome, int[] T2Chromosome, sbyte[][][] ConvolutionChromosome);
+internal static class Population
+{
+  public static Pop[] GenerateInitialPopulation(in ParameterSchema parameter)
+  {
+    Pop[] initialPopulation = new Pop[parameter.PopulationSize];
+    for (int i = 0; i < parameter.PopulationSize; i++)
+    {
+      initialPopulation[i] = new Pop(
+        Chromosome.RandomlyGenerateTypeOneChromosome(),
+        Chromosome.RandomlyGenerateTypeTwoChromosome(),
+        Chromosome.RandomlyGenerateConvolutionChromosome(in parameter)
+        );
+    }
+
+    return initialPopulation;
+  }
+}

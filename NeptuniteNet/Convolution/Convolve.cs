e19cf7479447bcdf81bccb62392d17f809e11182
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-29 00:13:31 +0530

    Move padding function into convolution

--- NeptuniteNet/Convolution/Convolve.cs
+++ NeptuniteNet/Convolution/Convolve.cs
@@ -14,7 +14,7 @@ internal static class Convolve
     int width = matrix.Width;
 
     int[][] convolvedMatrix = new int[height][];
-    Image.Matrix paddedMatrix = Matrix.PadMatrix.WithZeros(in matrix);
+    Image.Matrix paddedMatrix = PadMatrixWithZeros(in matrix);
 
     for (int i = offset; i < height - offset; i++)
     {
@@ -47,5 +47,40 @@ internal static class Convolve
     }
 
     return pixelReplacement;
+  }
+
+  public static Image.Matrix PadMatrixWithZeros(in Image.Matrix matrix)
+  {
+    int[][] paddedMatrix = new int[matrix.Height][];
+    int height = matrix.Height;
+    int width = matrix.Width;
+    for (int i = 0; i < height; i++)
+    {
+      int[] row;
+      if (i == 0 || i == height - 1) { row = ConstructArrayWithZeros(width + 2); }
+      else { row = PadArrayWithZeros(matrix.ReadRow(i)); }
+      paddedMatrix[i] = row;
+    }
+
+    return new Image.Matrix(paddedMatrix);
+  }
+
+  private static int[] ConstructArrayWithZeros(in int length)
+  {
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++) { array[i] = 0; }
+    return array;
+  }
+
+  private static int[] PadArrayWithZeros(in int[] array)
+  {
+    int length = array.Length;
+    int[] paddedArray = new int[length + 2];
+
+    paddedArray[0] = 0;
+    paddedArray[length - 1] = 0;
+    for (int i = 1; i < length + 1; i++) { paddedArray[i] = array[i - 1]; }
+
+    return paddedArray;
   }
 }

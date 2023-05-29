0f29a5ebe738ce12b2b52932a38b579f3eb81c04
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-29 13:05:17 +0530

    Add zero operation ( 0A )

--- NeptuniteNet/Static/TypeOneFunction.cs
+++ NeptuniteNet/Static/TypeOneFunction.cs
@@ -34,7 +34,10 @@ internal static class TypeOneFunction
   private static readonly Func<int, int> Negate = (x) => -1 * x;
   private static void NegateInplace(ref Image.Matrix matrix) => Apply(ref matrix, Negate);
 
-  public enum TypeOneOperation { LogBase10, LogBase2, NaturalLog, Sin, Cos, Tan, Negate, }
+  private static readonly Func<int, int> Zero = (x) => 0 * x;
+  private static void ZeroInplace(ref Image.Matrix matrix) => Apply(ref matrix, Zero);
+
+  public enum TypeOneOperation { LogBase10, LogBase2, NaturalLog, Sin, Cos, Tan, Negate, Zero, }
   public static readonly int t1Count = Enum.GetNames(typeof(TypeOneOperation)).Length;
 
   public static void ApplyTypeOneOperationInplace(ref Image.Matrix matrix, in int operation)
@@ -48,6 +51,7 @@ internal static class TypeOneFunction
       case (int)TypeOneOperation.Cos: CosInplace(ref matrix); break;
       case (int)TypeOneOperation.Tan: TanInplace(ref matrix); break;
       case (int)TypeOneOperation.Negate: NegateInplace(ref matrix); break;
+      case (int)TypeOneOperation.Zero: ZeroInplace(ref matrix); break;
       default: throw new NotImplementedException();
     }
   }

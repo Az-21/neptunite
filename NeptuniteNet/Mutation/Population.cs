c7a85bc6f24bbc45f0d3c42696036708d249a642
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-05-30 00:09:21 +0530

    Fix deep copy issue (for real this time)

--- NeptuniteNet/Mutation/Population.cs
+++ NeptuniteNet/Mutation/Population.cs
@@ -7,12 +7,12 @@ internal static class Population
   public static void MutateTopFiftyAndReplaceBottomFiftyInplace(ref Pop[] parents, in ParameterSchema parameter)
   {
     int pops = parameter.PopulationSize;
-    for (int i = pops - 1; i < 2 * pops; i++)
+    for (int i = pops; i < 2 * pops; i++)
     {
       // Deep copy top 50% population's genes
       int[] t1Chromosome = DeepCopy(parents[i - pops + 1].T1Chromosome);
       int[] t2Chromosome = DeepCopy(parents[i - pops + 1].T2Chromosome);
-      Image.Matrix[] convolutionChromosome = DeepCopy(parents[i - pops + 1].ConvolutionChromosome);
+      Image.Matrix[] convolutionChromosome = DeepCopyImageMatrix(parents[i - pops + 1].ConvolutionChromosome);
 
       // Mutate
       Chromosome.MutateTypeOneChromosomeInplace(ref t1Chromosome, in parameter);
@@ -31,6 +31,31 @@ internal static class Population
 
     // Perform a deep copy of the bottom 50% of the elements
     Array.Copy(sourceArray, sourceArray.Length - length, copy, 0, length);
+    return copy;
+  }
+
+  private static Image.Matrix[] DeepCopyImageMatrix(in Image.Matrix[] matrices)
+  {
+    int layers = matrices.Length;
+    int rows = matrices[0].Height;
+    int cols = matrices[0].Width;
+
+    Image.Matrix[] copy = new Image.Matrix[layers];
+    for (int i = 0; i < layers; i++)
+    {
+      int[][] matrix = new int[rows][];
+      for (int j = 0; j < rows; j++)
+      {
+        int[] line = new int[cols];
+        for (int k = 0; k < cols; k++)
+        {
+          line[k] = matrices[i].ReadValue(j, k);
+        }
+        matrix[j] = line;
+      }
+      copy[i] = new Image.Matrix(matrix);
+    }
+
     return copy;
   }
 }
